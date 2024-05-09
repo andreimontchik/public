5980c08907c6864961f7e76985a500cefb57c74d
Andrei Montchik <andrei@montchik.net>
2024-05-09 15:54:19 -0500

    Add mutlipe instructions to  the RunFailOnDivisionTxn script

--- solana/programs/scripts/RunFailOnDivisionTxn.ts
+++ solana/programs/scripts/RunFailOnDivisionTxn.ts
@@ -1,4 +1,4 @@
-import { Keypair } from "@solana/web3.js";
+import { ComputeBudgetProgram, Keypair } from "@solana/web3.js";
 import * as fs from 'fs';
 import { serialize } from "borsh";
 import { Buffer } from "buffer";
@@ -37,16 +37,21 @@ const payloadSchema = new Map([
 
 async function main() {
 
-    if (process.argv.length !== 4) {
-        console.error('Required two numeric input parameters `Dividend` and `Remainder`!');
+    const scriptName = process.argv[1].split('/').pop()
+
+    console.log("Starting ", scriptName, " with ", process.argv[0]);
+
+    if (process.argv.length !== 5) {
+        console.error(`Invalid input arguments! Usage: ${scriptName} <Dividend> <Reminder> <NumberOfInstructions>`);
         process.exit(1);
     }
 
     const dividend: number = parseInt(process.argv[2], 10);
-    const remainder: number = parseInt(process.argv[3], 10);
-
     console.log('Dividend:', dividend);
+    const remainder: number = parseInt(process.argv[3], 10);
     console.log('Remainder:', remainder);
+    const numOfInstructions: number = parseInt(process.argv[4], 10);
+    console.log('NumberOfInstructions:', numOfInstructions);
 
     const CONNECTION_URL = "http://localhost:8899";
 
@@ -66,36 +71,61 @@ async function main() {
 
     let keys = [{ pubkey: payerKeyPair.publicKey, isSigner: true, isWritable: true }];
 
+
     for (let i = 1; i <= dividend; i++) {
+
         const transaction = new web3.Transaction({
             feePayer: payerKeyPair.publicKey,
         });
 
-        const payload = new Payload({
-            dividend,
-            divisor: i,
-            remainder,
+        // Add fees
+        const cuLimit = 5_000 * numOfInstructions;
+        const cuLimitInstruction = ComputeBudgetProgram.setComputeUnitLimit({
+            units: cuLimit,
         });
-        const payloadBuffer = Buffer.from(serialize(payloadSchema, payload));
+        transaction.add(cuLimitInstruction);
 
-        transaction.add(
-            new web3.TransactionInstruction({
+        const cuPrice = 100_000_000; // 1000 lamports
+        const cuPriceInstruction = ComputeBudgetProgram.setComputeUnitPrice({
+            microLamports: cuPrice,
+        });
+        transaction.add(cuPriceInstruction);
+
+        // Add instructions
+        for (let j = 0; j < numOfInstructions; j++) {
+
+            const payload = new Payload({
+                dividend,
+                divisor: i + j,
+                remainder,
+            });
+            const payloadBuffer = Buffer.from(serialize(payloadSchema, payload));
+            const transactionInstruction = new web3.TransactionInstruction({
                 keys: keys,
                 programId: programKeyPair.publicKey,
                 data: payloadBuffer,
-            }),
-        );
+            });
+            transaction.add(transactionInstruction);
+        }
 
         try {
-            console.log("Sending transaction with Payload: ", payload);
+
+            console.log(`${new Date(Date.now()).toLocaleString()}: Processing the transaction with divisor ${i} ...`);
+
+            const simulationResult = await connection.simulateTransaction(
+                transaction
+            );
+            console.log(`According to simulation, the transaction ${i} will consume ${simulationResult.value.unitsConsumed} out of ${cuLimit} CUs in fees.`);
+
+            console.log(`${new Date(Date.now()).toLocaleString()}: Submitting the transaction ${i}`);
             const txHash = await web3.sendAndConfirmTransaction(
                 connection,
                 transaction,
                 [payerKeyPair],
             );
-            console.log(`Transaction is confirmed. Signature:`, txHash);
+            console.log(`${new Date(Date.now()).toLocaleString()}: Transaction ${i} is confirmed. Signature: ${txHash}`);
         } catch (error) {
-            console.error(`Transaction is failed. Error: `, error.message);
+            console.error(`${new Date(Date.now()).toLocaleString()}: Transaction ${i} is failed. Error: ${error.message}`);
         }
     }
 }
